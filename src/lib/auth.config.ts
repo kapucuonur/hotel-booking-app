dbe8f5d294987ec2a0c92c147edecb5b216abb0c
Onur Kapucu <kapucuonur@hotmail.com>
2025-12-25 15:17:57 +0200

    add trustHost for vercel

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -44,4 +44,5 @@ export const authOptions: NextAuthOptions = {
         maxAge: 30 * 24 * 60 * 60, // 30 days
     },
     secret: process.env.NEXTAUTH_SECRET,
+    trustHost: true, // Required for Vercel deployment
 };
